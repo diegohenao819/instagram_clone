4fcfe70656898cd5704215e2b8361915eb26df0a
diegohenao819 <diegohenao819@gmail.com>
2022-01-20 20:21:25 -0500

    server January 20

--- instagram_server/index.js
+++ instagram_server/index.js
@@ -1,40 +1,37 @@
 const mongoose = require("mongoose");
-const {ApolloServer} = require("apollo-server")
+const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./gql/schema");
-const resolvers = require("./gql/resolver")
+const resolvers = require("./gql/resolver");
 
-require("dotenv").config({path: ".env"});
-
-
-
+require("dotenv").config({ path: ".env" });
 
 // ******************MONGOOSE BASE DE DATOS**********************************
-mongoose.connect(process.env.BBDD, 
-    {
+mongoose.connect(
+  process.env.BBDD,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useandFind & useIndex are not longer necessary  
-},
-(err, res)=>{
-    if(err){
-        console.log(err)
-    }else{
-        server();
+    // useandFind & useIndex are not longer necessary
+  },
+  (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      server();
     }
-}
+  }
 );
 // ******************MONGOOSE BASE DE DATOS**********************************
 
-
 // *********SERVIDOR ******************+
-function server(){
-    const serverApollo = new ApolloServer({
-        typeDefs,
-        resolvers,
-    });
-    serverApollo.listen().then((response)=>{
-        console.log("Servidor READY")
-        console.log(response.url)
-    })
+function server() {
+  const serverApollo = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  serverApollo.listen().then((response) => {
+    console.log("Servidor READY");
+    console.log(response.url);
+  });
 }
 // *********SERVIDOR ******************+
